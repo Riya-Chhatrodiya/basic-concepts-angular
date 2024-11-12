13712cd46af2c1b45185ed39bb851f0b492f9dd8
Riya-Chhatrodiya <riyachhatrodiya8102002@gamil.com>
2024-11-12 12:57:27 +0530

    RXJS Map Operators

--- src/app/booking/booking.component.ts
+++ src/app/booking/booking.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ConfigService } from '../services/config.service';
 import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { BookingService } from './booking.service';
+import { exhaustMap, mergeMap, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-booking',
@@ -15,18 +17,18 @@ export class BookingComponent implements OnInit {
     return this.bookingForm.get('guests') as FormArray
   } 
 
-  constructor(private configService:ConfigService, private fb:FormBuilder ) { }
+  constructor(private configService:ConfigService, private fb:FormBuilder, private bookingService: BookingService ) { }
 
   ngOnInit(): void {
     this.bookingForm = this.fb.group({
       roomId: new FormControl({value:'2', disabled:true},{ validators: [Validators.required]}),
-      guestEmail: ['', [Validators.required, Validators.email]],
+      guestEmail: ['', { updateOn:'blur' , validators:[Validators.required, Validators.email]}],
       checkinDate: [''],
       checkoutDate: [''],
       bookingStatus: [''],
       bookingAmount: [''],
       bookingDate: [''],
-      mobileNumber: [''],
+      mobileNumber: ['', { updateOn:'blur'}],
       guestName: ['',[Validators.required, Validators.minLength(5)]],
       address: this.fb.group({
         addressLine1: ['',[Validators.required]],
@@ -38,15 +40,57 @@ export class BookingComponent implements OnInit {
       }),
       guests: this.fb.array([this.addGuestControl()]),
       tnc: new FormControl(false, {validators: [Validators.requiredTrue]})
-    })
+    }, )
+    this.getBookingData();
+    // this.bookingForm.valueChanges.subscribe((data) => this.bookingService.bookRoom(data).subscribe((data) => {}) )
+
+    //--------------------------------------RXJS Map Operators --------------------------------------
+    // this.bookingForm.valueChanges.pipe(
+    //   mergeMap((data) => this.bookingService.bookRoom(data))
+    // ).subscribe((data) => {console.log(data)})
+
+    // this.bookingForm.valueChanges.pipe(
+    //   switchMap((data) => this.bookingService.bookRoom(data))
+    // ).subscribe((data) => {console.log(data)})
+
+    // this.bookingForm.valueChanges.pipe(
+    //   exhaustMap((data) => this.bookingService.bookRoom(data))
+    // ).subscribe((data) => {console.log(data)})
+    //-----------------------------------------------------------------------------------------------
   }
 
   addBooking() {
     console.log(this.bookingForm.getRawValue());
-    this.bookingForm.reset({
+    this.bookingService.bookRoom(this.bookingForm.getRawValue()).subscribe((data) => console.log(data))
+    // this.bookingForm.reset({
+    //   roomId: '2',
+    //   guestEmail: '',
+    //   checkinDate: '',
+    //   checkoutDate: '',
+    //   bookingStatus: '',
+    //   bookingAmount: '',
+    //   bookingDate: '',
+    //   mobileNumber: '',
+    //   guestName: '',
+    //   address:{
+    //     addressLine1: '',
+    //     addressLine2: '',
+    //     city: '',
+    //     state: '',
+    //     country: '',
+    //     zipCode: '',
+    //   },
+    //   guests : [],
+    //   tnc: false
+    // })
+    this.getBookingData();
+  }
+
+  getBookingData() {
+    this.bookingForm.patchValue({  //setValue --> you have to pass each and every value
       roomId: '2',
-      guestEmail: '',
-      checkinDate: '',
+      guestEmail: 'test@gmail.com',
+      checkinDate: new Date('10-Feb-2020'),
       checkoutDate: '',
       bookingStatus: '',
       bookingAmount: '',
